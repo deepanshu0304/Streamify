43b0d69b763f75ce4ea731a252fa764312a0014d
deepanshu0304 <deepanshu0322@gmail.com>
2024-03-31 18:57:50 +0530

    clerk authetication added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <p className="text-red-500 font-bold">This is Deepanshu Garg</p>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton
+      afterSignOutUrl="/"
+      />
+    </div>
+  );
 }

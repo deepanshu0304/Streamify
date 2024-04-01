5484348edbb67468c2462d28b3159755f12ea17c
deepanshu0304 <deepanshu0322@gmail.com>
2024-04-01 13:33:51 +0530

    prisma added

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import { Logo } from "./_components/logo";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex flex-col justify-center items-center">
+    <div className="h-full flex flex-col justify-center items-center space-y-6">
       <Logo />
       {children}
     </div>

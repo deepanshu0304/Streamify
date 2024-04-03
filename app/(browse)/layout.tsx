f79924630bf39b92caaed8b0b2a2b66717824d14
deepanshu0304 <deepanshu0322@gmail.com>
2024-04-03 18:22:01 +0530

    clerk-webhook added

---
+++ app/(browse)/layout.tsx
@@ -0,0 +1,9 @@
+const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <div className="flex h-full pt-20">{children}</div>
+    </>
+  );
+};
+
+export default BrowseLayout;

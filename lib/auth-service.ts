dfd5f892b03dfb33b9789e71d0472565d2d9443f
deepanshu0304 <deepanshu0322@gmail.com>
2024-11-13 01:36:44 +0530

    dashboard layout

--- lib/auth-service.ts
+++ lib/auth-service.ts
@@ -8,7 +8,6 @@ export const getSelf = async () => {
   if (!self || !self.username) {
     throw new Error("Unauthorized");
   }
-
   const user = await db.user.findUnique({
     where: {
       extrenalUserId: self.id,
@@ -16,6 +15,29 @@ export const getSelf = async () => {
   });
 
   if (!user) throw new Error("Not found");
+
+  return user;
+};
+
+export const getSelfByUsername = async (username: string) => {
+  const self = await currentUser();
+
+  if (!self || !self.username) {
+    throw new Error("Unauthorized");
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+  if (self.username !== user.username) {
+    throw new Error("Unauthorized");
+  }
 
   return user;
 };
